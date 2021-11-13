e192165e8a4bb7d550dcbce1f09bdf1ff9650ef6
Hugo Campos <88694712+HugoCamposArimathea@users.noreply.github.com>
2021-11-13 20:33:16 -0300

    Primeira parte KeyboardEvent

--- script/script.js
+++ script/script.js
@@ -23,3 +23,22 @@ function solve() {
     let solved = eval(equation);
     document.getElementById('output').innerHTML = solved;
 }
+
+const mapaTeclado = {
+    '0' : 'zero',
+    '1' : 'one',
+    '2' : 'two',
+    '3' : 'three',
+    '4' : 'four',
+    '5' : 'five',
+    '6' : 'six',
+    '7' : 'seven',
+    '8' : 'eight',
+    '9' : 'nine'
+}
+const mapearTeclado = (evento) => {
+    let tecla = evento.key;
+    document.getElementById(mapaTeclado[tecla]).fordisplay(value);
+}
+document.addEventListener('keyup', mapearTeclado);
+    
