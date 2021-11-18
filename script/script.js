6e9b023428489d7f5ef5cfb6b7cc9b76e6aa5763
Hugo Campos <hugocamposarimathea@gmail.com>
2021-11-18 14:11:07 -0300

    Área do histórico

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ function removeZero() {
     if (value == "0") {
         value = " "
         document.getElementById('output').innerHTML = value;
-    }
+    };
 };
 
 //Cálculo de percentual 
@@ -17,37 +17,19 @@ function perc() {
     let value = document.getElementById('output').innerHTML;
     value = value / 100;
     document.getElementById('output').innerHTML = value;
-}
+};
 
 //Coloca o número escolhido no display
 function fordisplay(value) {
-    removeZero()
+    removeZero();
     document.getElementById('output').innerHTML += value;
-}
+};
 function solve() {
     removeZero()
     let equation = document.getElementById('output').innerHTML;
     let solved = eval(equation);
     document.getElementById('output').innerHTML = solved;
-}
+};
 
 
-//Bloco do KeyboardEvent
-const mapaTeclado = {
-    '0' : 'zero',
-    '1' : 'one',
-    '2' : 'two',
-    '3' : 'three',
-    '4' : 'four',
-    '5' : 'five',
-    '6' : 'six',
-    '7' : 'seven',
-    '8' : 'eight',
-    '9' : 'nine'
-}
-const mapearTeclado = (evento) => {
-    let tecla = evento.key;
-    document.getElementById(mapaTeclado[tecla]).fordisplay(value);
-}
-document.addEventListener('keyup', mapearTeclado);
     
